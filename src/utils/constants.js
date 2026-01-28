8fdd87de666010ab6b9f98ba6a99f38aecca399d
Nestor <nestorsaumeth20@gmail.com>
2026-01-28 15:13:32 -0500

    First commit. Old proyect, this time, better organized.

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const defaultColor = '#7174FA'
+
+export const includedColors = [
+    '#f00', '#0f0', '#00f', '#ff0', '#0ff', '#FF9100FF', '#00FFD0FF', '#6600FFFF',
+    '#8f16a4', '#4f0b0c', '#7174FA'
+]
